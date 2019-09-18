d49e9a3c456044a5922684db08e44b5a7ad7445a
LV <1475017874@qq.com>
2019-09-18 16:46:54 +0800

    lv1.2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.cou.println("吕金洋是个大傻子");
+        System.out.println("吕金洋是个大傻子");
+        System.out.println("txb is main 田傻逼");
     }
 }
