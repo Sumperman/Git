3889ccc1353d133725d2c98e7fb0b28a382eb272
fql <1253248392@qq.com>
2019-09-18 17:00:46 +0800

    fengql添加输出语句

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("吕金洋是个大傻子");
         System.out.println("txb is main 田傻逼");
+        System.out.println("田晓彬大帅哥");
     }
 }
