358946e788ae7909d58f6fc52916823baf6d5fa9
tianxb <952815417@qq.com>
2019-09-19 08:34:43 +0800

    添加了输出方法uuid

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,10 @@
+import java.util.UUID;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.cou.println("吕金洋是个大傻子");
+        System.out.println("吕金洋是个大傻子");
+        String s = UUID.randomUUID().toString();
+        System.out.println(s);
     }
 }
